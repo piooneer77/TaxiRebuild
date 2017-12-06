9c51c674319c240bd59a607a2422c5d80bddec7b
Piooneer77 <piooneer77@gmail.com>
2017-12-07 00:14:03 +0200

    111

--- src/main/java/com/syntaxerror/models/Account.java
+++ src/main/java/com/syntaxerror/models/Account.java
@@ -4,9 +4,7 @@ import javax.persistence.*;
 import java.io.File;
 import java.util.Set;
 
-@Entity
-@Table( name = "account_table" )
-public class Account {
+public abstract class Account {
 
     // <editor-fold defaultstate="collapsed" desc="Properties">
     @Id
@@ -29,34 +27,127 @@ public class Account {
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Constructors">
-    public Account() {
-    }
-
-    public Account(String firstName, String lastName, String phone, String mail, String user, String magic, Location currentLocation, Boolean isOnTrip, Boolean isLocked, Boolean isDeleted, Float rate, File faceImage, Integer tripCount, Set<Reports> reports) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.mail = mail;
-        this.user = user;
-        this.magic = magic;
-        this.currentLocation = currentLocation;
-        this.isOnTrip = isOnTrip;
-        this.isLocked = isLocked;
-        this.isDeleted = isDeleted;
-        this.rate = rate;
-        this.faceImage = faceImage;
-        this.tripCount = tripCount;
-        this.reports = reports;
-    }
 
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Getters">
+    public Integer getId() {
+        return Id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getMagic() {
+        return magic;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public Boolean getOnTrip() {
+        return isOnTrip;
+    }
+
+    public Boolean getLocked() {
+        return isLocked;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public Float getRate() {
+        return rate;
+    }
+
+    public File getFaceImage() {
+        return faceImage;
+    }
+
+    public Integer getTripCount() {
+        return tripCount;
+    }
+
+    public Set<Reports> getReports() {
+        return reports;
+    }
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Setters">
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setMagic(String magic) {
+        this.magic = magic;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public void setOnTrip(Boolean onTrip) {
+        isOnTrip = onTrip;
+    }
+
+    public void setLocked(Boolean locked) {
+        isLocked = locked;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public void setRate(Float rate) {
+        this.rate = rate;
+    }
+
+    public void setFaceImage(File faceImage) {
+        this.faceImage = faceImage;
+    }
+
+    public void setTripCount(Integer tripCount) {
+        this.tripCount = tripCount;
+    }
+
+    public void setReports(Set<Reports> reports) {
+        this.reports = reports;
+    }
     // </editor-fold>
 
 }
--- src/main/java/com/syntaxerror/models/Location.java
+++ src/main/java/com/syntaxerror/models/Location.java
@@ -9,15 +9,42 @@ public class Location {
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Constructors">
+    public Location() {
+    }
 
+    public Location(String name, double longitude, double latitude) {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Getters">
+    public String getName() {
+        return name;
+    }
 
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Setters">
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
     // </editor-fold>
 
 }
