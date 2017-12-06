ad6a8a87b91499aaa85c938d4a308711f7ded216
piooneer77 <piooneer77@gmail.com>
2017-12-06 19:51:03 +0200

    init 0.1

---
+++ src/main/java/com/syntaxerror/TaxiRebuildApplication.java
@@ -0,0 +1,12 @@
+package com.syntaxerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiRebuildApplication.class, args);
+	}
+}
