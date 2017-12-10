f0412c29ae7b9d512b0b3f78c6d615ac59a90dac
piooneer77 <piooneer77@gmail.com>
2017-12-10 15:18:01 +0200

    Karem Added the Roles Enum

--- src/main/java/com/syntaxerror/models/Roles.java
+++ src/main/java/com/syntaxerror/models/Roles.java
@@ -1,17 +1,26 @@
 package com.syntaxerror.models;
 
 public enum Roles {
+
+    // <editor-fold defaultstate="collapsed" desc="Nodes">
     Admin{
         @Override
         public Integer getAccountRoleNumber(){return 0;}
     },
+
     Driver{
         @Override
         public Integer getAccountRoleNumber(){return 1;}
     },
+
     Client{
         @Override
         public Integer getAccountRoleNumber(){return 2;}
     };
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Abstract Methods">
     public abstract Integer getAccountRoleNumber();
+    // </editor-fold>
+
 }
